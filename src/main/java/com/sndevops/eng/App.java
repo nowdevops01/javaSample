37048ccc5aeb6a3588aacba40b75a11a0735bb2b
rrasane <47263869+rrasane@users.noreply.github.com>
2020-04-22 17:29:48 -0700

    Fixed Issues

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -13,12 +13,5 @@ public class App
         int i =0;
         i++;
         System.out.println( "Hello test devops again : " + i);
-        
-        callModel("Slacker");
     }
-    
-    public callModel(String str) {
-        System.out.println( "My Job : " + str);
-    }
-    // Comment for testing
 }
