6a6344edafffd75e5b427c36f6c99f69f7341359
rrasane <47263869+rrasane@users.noreply.github.com>
2020-04-22 17:05:38 -0700

    BM-4: Added Model

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -13,6 +13,12 @@ public class App
         int i =0;
         i++;
         System.out.println( "Hello test devops again : " + i);
+        
+        callModel("Slacker");
+    }
+    
+    public static callModel(String str) {
+        System.out.println( "My Job : " + str);
     }
     // Comment for testing
 }
